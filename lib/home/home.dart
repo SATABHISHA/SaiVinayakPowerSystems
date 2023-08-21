944f07c4fc6f4c0b3b1b9fad85378ef521d94cd1
ArbSoftware <satabhishar@arbsoft.comm>
2023-08-21 16:22:22 +0530

    modified

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -9,27 +9,33 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 350,
-              child: Stack(
-                children: [
-                  Image.asset('images/landingpageimage.jpg', width: MediaQuery.of(context).size.width, fit: BoxFit.fill),
-                  DefaultTextStyle(style: TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
-                  ),child: Center(child: AnimatedTextKit(totalRepeatCount: 1,animatedTexts: [TyperAnimatedText('Sai Vinayak Power Systems')],))),
-                ],
-              ),
-            ),
+        body: ListView(
+          shrinkWrap: true,
 
+          children: [
             Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 350,
+                  child: Stack(
+                    children: [
+                      Image.asset('images/landingpageimage.jpg', width: MediaQuery.of(context).size.width, fit: BoxFit.fill),
+                      DefaultTextStyle(style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.white,
+                      ),child: Center(child: AnimatedTextKit(totalRepeatCount: 1,animatedTexts: [TyperAnimatedText('Sai Vinayak Power Systems')],))),
+                    ],
+                  ),
+                ),
 
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+
+                    )
+                  ],
                 )
               ],
             )
